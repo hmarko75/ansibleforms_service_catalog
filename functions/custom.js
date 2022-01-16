6ec46ab16ecb9421b29382b6e7f0e5d0453419a9
root <root@centos2.demo.netapp.com>
2022-01-16 11:25:55 +0000

    svm

---
+++ functions/custom.js
@@ -0,0 +1,46 @@
+const logger=require("../lib/logger");
+
+// Add you custom functions here.  You can add the logger optionally
+// logger supports .error .warn .info .debug .silly
+// the functions do not need to start with fn, but we all like consistancy
+
+exports.fnSum = function(a,b) {
+  logger.silly("[fnSum] sum is happening")
+  return a+b
+};
+exports.fnMultiply = function(a,b) {
+  logger.silly("[fnMultiply] multiply is happening")
+  return a*b
+};
+
+exports.fnIpRangeToList = function (data) {
+  //const data = '10.5.15.[22-25],10.5.16.[35-37],10.5.17.20';
+  // Placeholder for the final result
+  const result = [];
+  
+  // Use the split function to get all your Ips in an array
+  const ips = data.split(',');
+  
+  // Use the built in forEach function to iterate through the array
+  ips.forEach(ip => {
+    // This regular expression will match the potential extremities of the range
+    const regexp = /[0-9]+\.[0-9]+\.[0-9]+\.\[(.*)\-(.*)\]/;
+    const match = regexp.exec(ip);
+    
+    // If it's a range
+    if (match && match.length && match[1] && match[2]) {
+        // Iterate through the extremities and add the IP to the result array
+        for (let i = parseInt(match[1]); i <= parseInt(match[2]); i ++) {
+          result.push(ip.match(/[0-9]+\.[0-9]+\.[0-9]+\./i)[0] + i);
+        }
+    } else { // If it is a single IP
+      // Add to the results
+      result.push(ip);
+    }
+  })
+  //console.log(result);
+  logger.debug("[fnIpRangetoList] translated iprange:"+data+" to list:"+result);   
+  return(result);
+};
+     
+//console.log(exports.fnIpRangeToList('10.5.15.[22-25],10.5.16.[35-37],10.5.17.20')); 
